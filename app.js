9c63f03b9d736ae2e17de525c2ccb409de0861fd
viktorkhyzhniak <viktor.khyshnyak@gmail.com>
2021-10-25 23:17:36 +0300

    models ready

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const analyticsRoutes = require('./routes/analytics');
 const authRoutes = require('./routes/auth');
 const categoryRoutes = require('./routes/category');
 const orderRoutes = require('./routes/order');
 const positionRoutes = require('./routes/position');
+
 const app = express();
+
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(require('cors')());
 
 app.use('/api/analytics', analyticsRoutes)
 app.use('/api/auth', authRoutes)
