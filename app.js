b1450087a81661034941eb610f0efd00e468f5c7
viktorkhyzhniak <viktor.khyshnyak@gmail.com>
2021-10-30 15:00:33 +0300

    added mongo

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const analyticsRoutes = require('./routes/analytics');
 const authRoutes = require('./routes/auth');
@@ -7,7 +8,13 @@ const categoryRoutes = require('./routes/category');
 const orderRoutes = require('./routes/order');
 const positionRoutes = require('./routes/position');
 
+const keys = require('./config/keys');
+
 const app = express();
+
+mongoose.connect(keys.mongoUri)
+    .then(() => console.log('----MONGO DB CONNECTED----'))
+    .catch((err) => console.error('----MONGO DB ERROR----', err))
 
 app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoUri: 'mongodb+srv://enviti:rqsZRAwrwO0YQw1W@cluster0.adirv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
