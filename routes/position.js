57951374030a0718a73ac44225bcc117aa082b7e
viktorkhyzhniak <viktor.khyshnyak@gmail.com>
2021-11-06 20:03:57 +0200

    fix update

--- routes/position.js
+++ routes/position.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/:categoryId', passport.authenticate('jwt', { session: false }), controller.getByCategoryId)
 router.post('/', passport.authenticate('jwt', { session: false }), controller.create)
-router.patch('/:id`', passport.authenticate('jwt', { session: false }), controller.update)
+router.patch('/:id', passport.authenticate('jwt', { session: false }), controller.update)
 router.delete('/:id', passport.authenticate('jwt', { session: false }), controller.remove)
 
 module.exports = router;
